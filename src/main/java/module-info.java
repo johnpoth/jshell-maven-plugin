ba68bf757db40a68a6f307df6c17094d855ca166
jpoth <poth.john@gmail.com>
2018-03-09 18:19:27 +0100

    add runtime classpath elements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module com.github.johnpoth.jshell {
+
    requires jdk.jshell;
+   /**not modules yet*/
+   requires maven.plugin.api;
+   requires maven.plugin.annotations;
 
    exports com.github.johnpoth;
 
